0f561cce98673cffb1790783e0ff41cde0ea8047
mttraore <marytra22@gmail.com>
2022-09-08 13:12:46 +0000

    mon-commit

---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.css']
+})
+export class AppareilComponent implements OnInit {
+
+AppareilName = 'Mon premier appareil';
+AppareilStatus = 'Bonne';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  getStatus(){
+    return this.AppareilStatus;
+  }
+
+}
